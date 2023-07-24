45aaa4547959b355f917c81f9b68e27719126058
changhoonhahn <changhoon.hahn@princeton.edu>
2023-07-24 13:18:02 -0400

    more flexible data

--- src/dusty/data.py
+++ src/dusty/data.py
@@ -1,25 +1,31 @@
 '''
 '''
+import os
 import numpy as np 
 import astropy.units as u
 from astropy.io import fits
 from astropy.table import Table
 
 
+if os.path.isdir('/scratch/gpfs/chhahn/dusty/'): 
+    dat_dir = '/scratch/gpfs/chhahn/dusty/'
+else: 
+    dat_dir = '/Users/chahah/data/dusty/'
+
 
 def nihao(filters, dust=False): 
     '''
     '''
     # read galaxies
-    gals = Table.read('/scratch/gpfs/chhahn/dusty/nihao-integrated-seds.fits', hdu=1)
+    gals = Table.read(os.path.join(dat_dir, 'nihao-integrated-seds.fits'), hdu=1)
 
     # read SEDS
-    wave = fits.open('/scratch/gpfs/chhahn/dusty/nihao-integrated-seds.fits')[3].data
+    f = fits.open(os.path.join(dat_dir, 'nihao-integrated-seds.fits'))
+
+    wave = f[3].data
     
-    if dust: 
-        seds = fits.open('/scratch/gpfs/chhahn/dusty/nihao-integrated-seds.fits')[4].data
-    else: 
-        seds = fits.open('/scratch/gpfs/chhahn/dusty/nihao-integrated-seds.fits')[5].data
+    if dust: seds = f[4].data
+    else: seds = f[5].data
 
     zred = 0.022229
     # observed frame z
@@ -28,5 +34,5 @@ def nihao(filters, dust=False):
     fl = seds / (3.34e4) / wavez**2 # ergs/s/cm^2/A
 
     maggies = np.array([np.array(list(arr))
-                        for arr in filters.get_ab_maggies(fl, wavelength=wave).as_array()]) * 1e9
+        for arr in filters.get_ab_maggies(fl, wavelength=wave).as_array()]) * 1e9
     return gals, maggies 
