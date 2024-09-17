bfe9268eb87d77da595100303285ee9edda4272e
ChangHoon Hahn <attendee@hackweek.com>
2024-09-17 14:08:54 -0400

    dry-redshifting NIHAO galaxy SEDs. Fixed up luminosity scaling issue

--- src/dusty/__init__.py
+++ src/dusty/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 __author__ = "ChangHoon Hahn"
 __email__ = "changhoon.hahn@princeton.edu"
-__uri__ = "https://github.com/changhoonhahn/haloflow"
+__uri__ = "https://github.com/changhoonhahn/noah2"
 __license__ = "MIT"
-__description__ = "Python Package for haloflow"
-__copyright__ = "Copyright 2023 ChangHoon Hahn"
+__description__ = "Python Package for NOAH2"
+__copyright__ = "Copyright 2024 ChangHoon Hahn"
